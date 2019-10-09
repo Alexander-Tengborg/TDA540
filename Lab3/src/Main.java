8e613917f26a0a58f16a8427b6b480bd8e6bb31e
Alexander-Tengborg <Landervich@Gmail.com>
2019-10-09 23:31:38 +0200

    Uppgift 4
    Å annat gött

--- Lab3/src/Main.java
+++ Lab3/src/Main.java
@@ -1,23 +1,30 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(args[0]));
+
         SoundDevice device = new SoundDevice();
-        Song song = new Song(10);
-        song.add(MusicUtils.harmonic(-9, 0.4));
-        song.add(MusicUtils.harmonic(-9, 0.4));
-        song.add(MusicUtils.harmonic(-9, 0.4));
-        song.add(MusicUtils.harmonic(-5, 0.4));
-        song.add(MusicUtils.harmonic(-7, 0.4));
-        song.add(MusicUtils.harmonic(-7, 0.4));
-        song.add(MusicUtils.harmonic(-7, 0.4));
-        song.add(MusicUtils.harmonic(-4, 0.4));
-        song.add(MusicUtils.harmonic(-5, 0.4));
-        song.add(MusicUtils.harmonic(-5, 0.4));
-        song.add(MusicUtils.harmonic(-7, 0.4));
-        song.add(MusicUtils.harmonic(-7, 0.4));
-        song.add(MusicUtils.harmonic(-9, 1));
+        Song song = new Song(40);
+
+        //Tempot för country roads: ~65
+        //Allstar ~90
+        int tempo = Integer.parseInt(args[1]);
+
+        while(sc.hasNextLine()) {
+            String nextLine = sc.nextLine().trim();
+            if(nextLine.trim().equals("")) continue;
+
+            int pitch = Integer.parseInt(nextLine.split("\\s+")[0].trim());
+            double duration = Double.parseDouble(nextLine.split("\\s+")[1].trim()) * 60/(tempo*0.250);
+
+            song.add(MusicUtils.harmonic(pitch, duration));
+        }
+
         song.play(device);
-        song.save(device.getFormat(),new File("twotones.wav"));
+        //song.save(device.getFormat(),new File(args[0].split(".")[0] + ".wav"));
+        //song.save(device.getFormat(),new File("countryroads.wav"));
     }//main
 }//Main
