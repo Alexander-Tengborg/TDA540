a1ba29408f4a4941cf5c46f7d30983448457223d
Alexander-Tengborg <Landervich@Gmail.com>
2019-10-10 11:16:23 +0200

    Uppgift 2 (Klar)
    sobel blev WONKY

--- Lab4/src/MyColorProgram.java
+++ Lab4/src/MyColorProgram.java
@@ -1,9 +1,9 @@
 public class MyColorProgram { 
    public static void main(String[] args) throws Exception { 
       int[][][] original = ColorImage.read("mushroom.jpeg"); 
-      int[][][] manipulated = sharpenOne(original);
+      int[][][] manipulated = sobel(original);
       ColorImage.write("upDownMushroom.jpeg", manipulated); 
-      ColorImageWindow iw = new ColorImageWindow(original, manipulated); 
+      ColorImageWindow iw = new ColorImageWindow(original, manipulated);
    }//main 
 
    public static int[][][] upDown(int[][][] samples) { 
@@ -65,12 +65,8 @@ public class MyColorProgram {
       int[][][] newSamples = new int[samples.length][samples[0].length][3];
       for(int row = 0; row < samples.length; row++)
          for(int col = 0; col < samples[row].length; col++) {
-
-            for(int c = 0; c < samples[row][col].length; c++)
-               newSamples[row][col] = sharpenOneFilter(samples, row, col);
+            newSamples[row][col] = sharpenOneFilter(samples, row, col);
          }
-
-      //newSamples[row][col] = 255-samples[row][col];
       return newSamples;
    }
 
@@ -84,18 +80,100 @@ public class MyColorProgram {
          for(int curCol = col - 1; curCol <= (col + 1); curCol++) {
             if(curCol < 0 || curCol > (samples[row].length - 1) || (curCol == col && curRow == row)) continue;
             for(int i = 0; i < 3; i++) {
-               int newColor = samples[row][col][i];
-               if(newColor > 255) newColor = 255;
-               if(newColor < 0) newColor = 0;
+               int newColor = samples[curRow][curCol][i];
+
                newColors[i] -= newColor;
             }
-
-            /*newRed -= samples[curRow][curCol][0];
-            newGreen -= samples[curRow][curCol][1];
-            newBlue -= samples[curRow][curCol][2];*/
          }
       }
-      System.out.println("Old: " + samples[row][col][0] + "   New: " +  newColors[2]);
+
+      for(int i = 0; i < 3; i++) {
+         int newColor = newColors[i];
+
+         if(newColor > 255) newColor = 255;
+         if(newColor < 0) newColor = 0;
+
+         newColors[i] = newColor;
+      }
+
+      return newColors;
+   }
+
+   public static int[][][] sharpenTwo(int[][][] samples) {
+      int[][][] newSamples = new int[samples.length][samples[0].length][3];
+      for(int row = 0; row < samples.length; row++)
+         for(int col = 0; col < samples[row].length; col++) {
+            newSamples[row][col] = sharpenTwoFilter(samples, row, col);
+         }
+      return newSamples;
+   }
+
+   public static int[] sharpenTwoFilter(int[][][] samples, int row, int col) {
+      int[] newColors = new int[3];
+      for(int i = 0; i < 3; i++)
+         newColors[i] = 5*samples[row][col][i];
+
+      for(int curRow = row - 1; curRow <= (row + 1); curRow++) {
+         if(curRow < 0 || curRow > (samples.length - 1)) continue;
+         for(int curCol = col - 1; curCol <= (col + 1); curCol++) {
+            if(curCol < 0 || curCol > (samples[row].length - 1) || (curCol == col && curRow == row)) continue;
+            if(curCol != col && curRow != row) continue;
+            for(int i = 0; i < 3; i++) {
+               int newColor = samples[curRow][curCol][i];
+
+               newColors[i] -= newColor;
+            }
+         }
+      }
+
+      for(int i = 0; i < 3; i++) {
+         int newColor = newColors[i];
+
+         if(newColor > 255) newColor = 255;
+         if(newColor < 0) newColor = 0;
+
+         newColors[i] = newColor;
+      }
+      return newColors;
+   }
+
+   public static int[][][] sobel(int[][][] samples) {
+      int[][][] newSamples = new int[samples.length][samples[0].length][3];
+      for(int row = 0; row < samples.length; row++)
+         for(int col = 0; col < samples[row].length; col++) {
+            newSamples[row][col] = sobelFilter(samples, row, col);
+         }
+      return newSamples;
+   }
+
+   public static int[] sobelFilter(int[][][] samples, int row, int col) {
+      int[] newColors = new int[3];
+
+      int[][] xFilter = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+      int[][] yFilter = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+
+      double[] xColors = new double[3];
+      double[] yColors = new double[3];
+
+      for(int curRow = row - 1; curRow <= (row + 1); curRow++) {
+         if(curRow < 0 || curRow > (samples.length - 1)) continue;
+         for(int curCol = col - 1; curCol <= (col + 1); curCol++) {
+            if(curCol < 0 || curCol > (samples[row].length - 1) || (curCol == col && curRow == row)) continue;
+            int normRow = curRow - row + 1;
+            int normCol = curCol - col + 1;
+            for(int i = 0; i < 3; i++) {
+               xColors[i] += xFilter[normCol][normRow] * samples[curRow][curCol][i];
+               yColors[i] += yFilter[normRow][normCol] * samples[curRow][curCol][i];
+            }
+         }
+      }
+
+      for(int i = 0; i < 3; i++) {
+         double xColor = Math.pow(xColors[i], 2);
+         double yColor = Math.pow(yColors[i], 2);
+         int newColor = (int)Math.sqrt(xColor + yColor);
+         newColors[i] = newColor;
+      }
       return newColors;
    }
 }//MyColorProgram
